1b50f3d069943d6e3b55022916c1fa496d9175a1
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-02 21:29:39 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,27 @@
+dict = {'apple':200,'orange':300,'mango':400,'grapes':700}
+print(dict)
+print(dict['apple'])
+"""print using for loop it shows key in default"""
+# for i in dict:
+#     print(i)
+
+"""want to retreive  key only"""
+# for i in dict.keys():
+#     print(i)
+#
+"""want to print values"""
+# for i in dict.values():
+#     print(i)
+#
+"""want to print the whole key value """
+# for i in dict.items():
+#     print(i)
+
+"""change value """
+# dict['apple'] = 450
+# print(dict)
+
+"""delete value """
+# del(dict['apple'])
+print(dict)
+
