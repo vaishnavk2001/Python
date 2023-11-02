885d4859af223cd293bc19ee5f96ff978dbe9d46
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-02 21:31:24 +0530

    dictionary

--- dictionary.py
+++ dictionary.py
@@ -2,26 +2,26 @@ dict = {'apple':200,'orange':300,'mango':400,'grapes':700}
 print(dict)
 print(dict['apple'])
 """print using for loop it shows key in default"""
-# for i in dict:
-#     print(i)
+for i in dict:
+    print(i)
 
 """want to retreive  key only"""
-# for i in dict.keys():
-#     print(i)
-#
+for i in dict.keys():
+    print(i)
+
 """want to print values"""
-# for i in dict.values():
-#     print(i)
-#
+for i in dict.values():
+    print(i)
+
 """want to print the whole key value """
-# for i in dict.items():
-#     print(i)
+for i in dict.items():
+    print(i)
 
 """change value """
-# dict['apple'] = 450
-# print(dict)
+dict['apple'] = 450
+print(dict)
 
 """delete value """
-# del(dict['apple'])
+del(dict['apple'])
 print(dict)
 
