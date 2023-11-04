a7ba9eea0e82d713562a6b4527b7aca34748118c
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-04 11:15:36 +0530

    string function and formating

---
+++ string function.py
@@ -0,0 +1,13 @@
+"""string functions"""
+nw_string = 'string checking'
+print(nw_string.startswith('string'))
+print(nw_string.endswith('checking'))
+print(nw_string.upper())
+print(nw_string.lower())
+
+"""number functions"""
+nw_list = [0,2,5,3,8,4,3,1,1]
+print(max(nw_list))
+print(min(nw_list))
+print(nw_list.count(1))
+print(abs(-22))
---
+++ string_formatting.py
@@ -0,0 +1,3 @@
+list_1 = [10, 25, 36, 73]
+nw_list = "numbers:{0},{1},{2},{3}".format(list_1[3],list_1[1],list_1[2],list_1[0])
+print(nw_list)
